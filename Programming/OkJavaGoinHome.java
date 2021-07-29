80b1908906831140df39f847a1458092b85dbe39
MOONLASTBLUE <87862780+MOONLASTBLUE@users.noreply.github.com>
2021-07-29 17:26:32 +0900

    commit

---
+++ Programming/OkJavaGoinHome.java
@@ -0,0 +1,35 @@
+import org.opentutorials.iot.Elevator; 
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+//import를 통해 Elevator를 짧게 써서 부를 수 있음
+
+public class OkJavaGoinHome {
+
+	public static void main(String[] args) {
+		String id = "JAVA APT 507";
+		
+		// Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// Light on
+		Lighting hallLamp = new Lighting(id+" / HallLamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / floorLamp");
+		floorLamp.on();
+		
+		/***
+		 * 디버거(벌레모양 아이콘)
+		 * 디버거를 이용하면 프로그램을 한 줄씩 실행 시킬 수 있음
+		 * 실행되는 순간에 애플리케이션 내에 변수 상태를 하나씩 체크가능
+		 * 
+		 */
+		 
+	}
+
+}
---
+++ Programming/Program.java
@@ -0,0 +1,17 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+	  /***1억줄이라 생각해보기, 하루에도 수십번 실행되고, 많은 사람들이 사용하는 코드라면?
+		컴퓨터가 순차적으로 해당 코드를 사용함에 따라 자동화가 진행됨
+		프로그래밍으로 얻을 수 있는 효과
+		-> 순차적으로 실행이 됨을 통해서 사람이 잘 못하는 일을 기계에게 위임해서 자동화
+	*
+	*/
+		
+	}
+
+}
