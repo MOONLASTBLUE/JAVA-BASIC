c41edbbcb55764b6ff87b75f1d788862859d0d70
MOONLASTBLUE <87862780+MOONLASTBLUE@users.noreply.github.com>
2021-08-01 22:27:59 +0900

    commit

---
+++ MyJavaProject/src/day5/Compile.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class Compile {
+
+	public static void main(String[] args) {
+		/***
+		 *  Compile
+		 *  - JAVA 확장자가 붙은 소스 코드를 클래스 확장자가 붙은 실행 파일로 바꾸는 것
+		 *  Run
+		 *  - 클래스 확장자가 붙은 파일을 실행하는 것
+		 *  Input
+		 *  - 실행할때, 입력값을 주는 것. 입력값에 따라서 다르게 동작하는 출력값을 만드는 것
+		 * 
+		 */
+	
+
+	}
+
+}
